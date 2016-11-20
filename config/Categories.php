2e5156082f443c30064eca5d9df4148c63925265
tgarriz <tgarriz@gmail.com>
2016-11-20 19:37:12 -0300

    primer commit

---
+++ config/Categories.php
@@ -0,0 +1,38 @@
+<?php
+
+Class Categories {
+
+    static function get($categ) {
+        $categories = array(
+            "Geodesia" => "GEODESIA E IMG.SATELITALES",
+            //"grandesobras" => "GRANDES OBRAS",
+            "fim" => "OBRAS F.I.M.",
+	    "bagsa" => "BAGSA",
+            "Ada" => "ADA",
+            "Spar" => "SPAR",
+            "Dipac" => "DIPAC",
+            "Vialidad" => "VIALIDAD",
+            "Secciones" => "SECC. ELECTORALES 2015",
+            "Censo2010" => "CENSO 2010",
+            "Red_a" => "RED DE AGUA",
+            "Red_c" => "RED DE CLOACA",
+            "IGN" => "IGN",
+            "habitat" => "SUBS. TIERRAS URB. Y VIVIENDA",
+            "dpa" => "DPA",
+            "energia" => "ENERGIA",
+            "gasoducto" => "GASODUCTOS"
+            //"Oceba" => "OCEBA",            
+            //"Salud" => "SALUD".
+            //"Pluri_c" => "PLURIANUAL CLOACA",
+            //"Pluri_a" => "PLURIANUAL AGUA",
+            //"Pluri_arq" => "PLURIANUAL ARQUITECTURA",
+            //"urbasig" => "UrbaSig",
+            //"arba" => "ARBA"
+        );
+                
+        return $categories[$categ];
+    }
+
+}
+
+?>
